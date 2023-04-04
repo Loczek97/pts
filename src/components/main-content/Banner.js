547ccd61b951660a07e5a9bd67726751fa577452
Michał Szymański <szymanski.m698@gmail.com>
2023-04-04 21:51:59 +0200

    some little changes

--- src/components/main-content/Banner.js
+++ src/components/main-content/Banner.js
@@ -7,9 +7,9 @@ export default function Banner() {
             id='banner'
             style={{ backgroundImage: `url(${background})` }}
             className='z-10 bg-cover bg-center bg-fixed h-screen flex items-center justify-center top=-5 before:content-[""] before:w-full before:h-full before:bg-black before:opacity-60 before:absolute'>
-            <h1 className='text-center text-3xl md:text-4xl font-bold text-white md:p-20 z-20 flex flex-col'>
-                <span className='py-3'>Usługi transportowe dostosowane do</span>
-                <span className='text-yellow-400 underline decoration-solid py-3'>Twoich</span>
+            <h1 className='text-center text-3xl md:text-4xl font-bold text-white z-20 flex flex-col'>
+                <span className='py-3'>Usługi ziemne dopasowane</span>
+                <span className='special py-3'>do Twoich</span>
                 <span className='py-3'>potrzeb</span>
             </h1>
         </section>
--- src/components/main-content/Services.js
+++ src/components/main-content/Services.js
@@ -6,46 +6,48 @@ export default function Services() {
   return (
     <>
       <section id='services'>
-        <div className="bg-gray-800 flex flex-col md:flex-row">
-          <div className="flex justify-evenly items-center px-8 py-6 text-white w-full md:w-1/2">
-            <p className='md:pl-8'>
+        <div className="bg-gray-800 grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="flex justify-center items-center px-8 py-6 text-white w-[90%]">
+            <p className='md:pl-5'>
               lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
               ultricies, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec
               tincidunt nisl nisl vel nisl. Nullam ultricies, nisl nec ultricies
               lacinia, nisl nisl aliquet nisl, nec tincidunt nisl nisl vel nisl.
             </p>
           </div>
-          <div className="flex justify-center items-center px-8 py-4 w-full md:w-1/2">
+          <div className="flex justify-center items-center px-8 py-4">
             <img src={img} alt="test" className='rounded-md shadow-md w-[90%] h-[90%] shadow-gray-700 hover:scale-[1.02] hover:shadow-lg hover:shadow-gray-700 transition-all ease-in-out duration-300' />
           </div>
         </div>
 
         <Divider />
 
-        <div className="bg-gray-800 flex flex-col md:flex-row-reverse">
-          <div className="flex justify-center items-center px-8 py-4 text-white w-full md:w-1/2">
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            ultricies, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec
-            tincidunt nisl nisl vel nisl. Nullam ultricies, nisl nec ultricies
-            lacinia, nisl nisl aliquet nisl, nec tincidunt nisl nisl vel nisl.
-          </div>
-          <div className="flex justify-center items-center px-8 py-4 w-full md:w-1/2">
+        <div className="bg-gray-800 grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="flex justify-center items-center px-8 py-4">
             <img src={img} alt="test" className='rounded-md shadow-md w-[90%] h-[90%] shadow-gray-700 hover:scale-[1.02] hover:shadow-lg hover:shadow-gray-700 transition-all ease-in-out duration-300' />
           </div>
-        </div>
-
-        <Divider />
-
-        <div className="bg-gray-800 flex flex-col md:flex-row">
-          <div className="flex justify-evenly items-center px-8 py-6 text-white w-full md:w-1/2">
-            <p className='md:pl-8'>
+          <div className="flex justify-center items-center px-8 py-6 text-white w-[90%]">
+            <p className='md:pl-5'>
               lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
               ultricies, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec
               tincidunt nisl nisl vel nisl. Nullam ultricies, nisl nec ultricies
               lacinia, nisl nisl aliquet nisl, nec tincidunt nisl nisl vel nisl.
             </p>
           </div>
-          <div className="flex justify-center items-center px-8 py-4 w-full md:w-1/2">
+        </div>
+
+        <Divider />
+
+        <div className="bg-gray-800 grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="flex justify-center items-center px-8 py-6 text-white w-[90%]">
+            <p className='md:pl-5'>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              ultricies, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec
+              tincidunt nisl nisl vel nisl. Nullam ultricies, nisl nec ultricies
+              lacinia, nisl nisl aliquet nisl, nec tincidunt nisl nisl vel nisl.
+            </p>
+          </div>
+          <div className="flex justify-center items-center px-8 py-4">
             <img src={img} alt="test" className='rounded-md shadow-md w-[90%] h-[90%] shadow-gray-700 hover:scale-[1.02] hover:shadow-lg hover:shadow-gray-700 transition-all ease-in-out duration-300' />
           </div>
         </div>
